ea5877d4e0b19c60ea813df3254ea4667bc3948e
Joel Montes de Oca <JoelMontes01@gmail.com>
2014-11-03 22:56:11 -0500

    Added a basic Qt window for testing.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 __author__ = 'Joel Montes de Oca'
 
+import sys
+from PySide import QtGui
 import pyTalkManagerDatabase as DB
 import pyTalkManager as TM
 
@@ -9,6 +11,16 @@ import pyTalkManager as TM
 def main():
 
     firstrun = TM.configGet('APP', 'FirstTimeRunning')
+    app = QtGui.QApplication(sys.argv)
+
+    wid = QtGui.QWidget()
+    wid.resize(250, 150)
+    wid.setWindowTitle('pyTalkManager')
+    wid.show()
+
+    sys.exit(app.exec_())
+
+
 
 if __name__ == '__main__':
     main()
