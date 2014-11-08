8cffa9f42773e5ec46ea008d38f7b55387e69915
Joel Montes de Oca <JoelMontes01@gmail.com>
2014-11-07 21:05:35 -0500

    Added DatabaseWindow

--- gui/DatabaseWindow.py
+++ gui/DatabaseWindow.py
@@ -2,39 +2,48 @@
 
 # Form implementation generated from reading ui file 'DatabaseWindow.ui'
 #
-# Created: Fri Nov  7 16:50:11 2014
+# Created: Fri Nov  7 21:02:42 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(169, 140)
-        self.gridLayout = QtGui.QGridLayout(Dialog)
+class Ui_DatabaseWindow(object):
+    def setupUi(self, DatabaseWindow):
+        DatabaseWindow.setObjectName("DatabaseWindow")
+        DatabaseWindow.resize(144, 128)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(DatabaseWindow.sizePolicy().hasHeightForWidth())
+        DatabaseWindow.setSizePolicy(sizePolicy)
+        DatabaseWindow.setSizeGripEnabled(False)
+        self.gridLayout = QtGui.QGridLayout(DatabaseWindow)
         self.gridLayout.setObjectName("gridLayout")
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.gridLayout.addItem(spacerItem, 0, 2, 1, 1)
-        self.pushButton_3 = QtGui.QPushButton(Dialog)
+        self.pushButton_3 = QtGui.QPushButton(DatabaseWindow)
         self.pushButton_3.setObjectName("pushButton_3")
-        self.gridLayout.addWidget(self.pushButton_3, 2, 1, 1, 1)
-        self.pushButton_2 = QtGui.QPushButton(Dialog)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.gridLayout.addWidget(self.pushButton_2, 3, 1, 1, 1)
-        self.pushButton = QtGui.QPushButton(Dialog)
+        self.gridLayout.addWidget(self.pushButton_3, 2, 2, 1, 1)
+        self.pushButton = QtGui.QPushButton(DatabaseWindow)
         self.pushButton.setObjectName("pushButton")
-        self.gridLayout.addWidget(self.pushButton, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton, 0, 2, 1, 1)
+        self.pushButton_2 = QtGui.QPushButton(DatabaseWindow)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.gridLayout.addWidget(self.pushButton_2, 1, 2, 1, 1)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 1, 0, 1, 1)
         spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.gridLayout.addItem(spacerItem1, 0, 0, 1, 1)
+        self.gridLayout.addItem(spacerItem1, 1, 3, 1, 1)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(DatabaseWindow)
+        QtCore.QMetaObject.connectSlotsByName(DatabaseWindow)
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Database", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_3.setText(QtGui.QApplication.translate("Dialog", "Crate Backup", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_2.setText(QtGui.QApplication.translate("Dialog", "Load Backup", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("Dialog", "New Database", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, DatabaseWindow):
+        DatabaseWindow.setWindowTitle(QtGui.QApplication.translate("DatabaseWindow", "Database", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_3.setToolTip(QtGui.QApplication.translate("DatabaseWindow", "Load a backup.", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_3.setText(QtGui.QApplication.translate("DatabaseWindow", "Load Backup", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setToolTip(QtGui.QApplication.translate("DatabaseWindow", "Create a new database.", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("DatabaseWindow", "New Database", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_2.setToolTip(QtGui.QApplication.translate("DatabaseWindow", "Backup your data.", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_2.setText(QtGui.QApplication.translate("DatabaseWindow", "Backup", None, QtGui.QApplication.UnicodeUTF8))
 
