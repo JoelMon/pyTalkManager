32f0da065cb68473303060ec8bc79565a4ea50ea
Joel Montes de Oca <JoelMontes01@gmail.com>
2014-11-11 18:04:42 -0500

    Added feature to init the database on the first run.
    When the user runs pyTalkManager for the first time, a dialogbox asks the user for a location to save the file. It then takes the location and saves it to the config.ini file along with switching the firsttimerunning key to False. A few other addtions were made along the way as well, for example, adding the configSet function for setting values in config.ini.

--- pyTalkManager.py
+++ pyTalkManager.py
@@ -3,12 +3,16 @@ __author__ = 'Joel Montes de Oca'
 import sys
 import configparser
 import sqlite3
+from PySide import QtGui
+
 
 version = '0.1'
 name    = 'pyTalkManager'
 configLocation = './config.ini'
 
 class db:
+
+    path = ''
 
     def dbinit():
         """Initialize SQLite database
@@ -18,7 +22,7 @@ class db:
         new SQLite database with all the needed tables and fields used by PyTalkManager.
         """
 
-        connection = sqlite3.connect('database.db') # hardcoded for now
+        connection = sqlite3.connect(db.path)
         c = connection.cursor()
 
         c.execute('''CREATE TABLE Assignment (
@@ -117,6 +121,36 @@ def configGet(section, key):
 
     return config[section].get(key)
 
+
+def configSet(section, key, value):
+    """
+    Takes arguments for the config.ini file and writes to file.
+    """
+
+    # Testing the file with exceptions rather than os.access() due to security risk.
+    # Source: https://docs.python.org/3.4/library/os.html#os.X_OK
+    try:
+        open(configLocation)
+    except FileNotFoundError:
+        messagebox.showerror("File config.ini was not found.", "The config.ini file was not found. "
+                                                               "Something may have gone wrong with the installation. "
+                                                               "Make sure the config.ini file is located in the "
+                                                               "application's root directory.")
+        print('The config.ini file was not found.')
+        quit()
+    except PermissionError:
+        messagebox.showerror('Permission problem.', "You do not have sufficient permission for the config.ini file. "
+                                                    "Fix the problem then run %s again." % name)
+        print('You do not have permission to read the config.ini file.')
+        quit()
+
+    config = configparser.ConfigParser()
+    config.read(configLocation)
+    config.set(section, key, value)
+    file = open(configLocation, 'w')
+    config.write(file)
+
+
 def buttonTest():
     """
     Test if a command works by printing a confirmation.
@@ -135,9 +169,29 @@ def firstRunCheck():
     """
 
     first_run = configGet('APP', 'FirstTimeRunning')
+
     if first_run == 'True':
-        print(first_run)
-        print('This is the first time pyTalkManager runs.')
-    else:
-        print(first_run)
-        print('This is not the first time pyTalkmanager runs.')
+
+        message_box = QtGui.QMessageBox()
+        message_box.setWindowTitle("First time running pyTalkManager")
+        message_box.setText("This is the first time you run pyTalkManager.\n\n"
+                                "The next window you will be asked to choose\n"
+                                "a location where you would like to save\n"
+                                "pyTalkManager's database.")
+        message_box.exec_()
+        file_name = QtGui.QFileDialog.getSaveFileName(None, "Save New Database", "New_Database.tdb",
+                                                      "pyTalkManager Database *.tdb")
+        if file_name[0] == '':
+            message_box = QtGui.QMessageBox()
+            message_box.setWindowTitle("Warning")
+            message_box.Warning
+            message_box.setText("You did not select a location to save the database.\n"
+                                "Run pyTalkManager again and select a location to save.")
+            message_box.exec_()
+            quit()
+
+        else:
+            db.path = file_name[0]
+            db.dbinit()
+            configSet('APP', 'firsttimerunning', 'False')
+            configSet('DB', 'location', db.path)
