4893f57adf0248cf651e50fe0dc130bcacf2d9c0
Joel Montes de Oca <JoelMontes01@gmail.com>
2014-11-14 11:28:16 -0500

    Both BrotherWindow and AddBrotherWindow have been added to pyTalkManager.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import sys
 # Importation of GUIs
 import gui.MainWindow
 import gui.DatabaseWindow
+import gui.BrotherWindow
+import gui.AddBrotherWindow
 
 
 class MainWindow(QtGui.QMainWindow, gui.MainWindow.Ui_MainWindow):
@@ -18,11 +20,16 @@ class MainWindow(QtGui.QMainWindow, gui.MainWindow.Ui_MainWindow):
         tm.firstRunCheck()
 
         # Tool bar actions
-        self.actionDatabase.triggered.connect(self.show_database_dialog)
+        self.actionDatabase.triggered.connect(self.show_database_window)
+        self.actionBrothers.triggered.connect(self.show_brother_window)
 
-    def show_database_dialog(self):
-        self.dbwindow = DatabaseWindow()
-        self.dbwindow.show()
+    def show_database_window(self):
+        self.db_window = DatabaseWindow()
+        self.db_window.show()
+
+    def show_brother_window(self):
+        self.bro_window = BrotherWindow()
+        self.bro_window.show()
 
 
 class DatabaseWindow(QtGui.QDialog, gui.DatabaseWindow.Ui_DatabaseWindow):
@@ -31,6 +38,24 @@ class DatabaseWindow(QtGui.QDialog, gui.DatabaseWindow.Ui_DatabaseWindow):
         super(DatabaseWindow, self).__init__(parent)
         self.setupUi(self)
 
+
+class BrotherWindow(QtGui.QDialog, gui.BrotherWindow.Ui_BrotherWindow):
+
+    def __init__(self, parent=None):
+        super(BrotherWindow, self).__init__(parent)
+        self.setupUi(self)
+
+        self.button_add.clicked.connect(self.show_add_brother_window)
+
+    def show_add_brother_window(self):
+        self.add_bro_window = AddBrotherWindow()
+        self.add_bro_window.show()
+
+class AddBrotherWindow(QtGui.QDialog, gui.AddBrotherWindow.Ui_AddBrotherWindow):
+
+    def __init__(self, parent=None):
+        super(AddBrotherWindow, self).__init__(parent)
+        self.setupUi(self)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
