7c984d76ccb80bc6065f505fb9f211bf68bcd494
Joel Montes de Oca <JoelMontes01@gmail.com>
2014-11-18 23:24:31 -0500

    Improved the add_congregation method.
    The method now checks to make sure all required fields were entered by the user before committing data.

--- congregation.py
+++ congregation.py
@@ -13,8 +13,34 @@ class Congregation:
         return DB.return_pass_sql(None, sql)
 
     def add_congregation(self, columns, values):
+        """Method that adds a new congregation to the database.
 
+         First checks to make sure all required
+         fields were entered by the user. If the user failed to enter
+         a required field then do not commit data to the database
+         and return a list of missing fields.
 
-        DB.add_item(None, 'Congregation', columns, values)
+         :var required_fields: a list of required fields
+         :var missing_fields: a list of required fields found to be empty
+        :param columns: a list of all columns in the Congregation table
+        :param values: a list of user entered data for each column
+        :return: Return True if all required fields were entered
+                 otherwise return False.
 
-        return True
+        """
+
+        required_fields = ['name', 'street', 'city', 'state', 'zip']
+
+        combine = zip(columns, values)
+
+        missing_fields = []
+        for x in combine:
+            if x[0] in required_fields and x[1] == '':
+                missing_fields.append(x[0])
+
+        if missing_fields == []:
+            DB.add_item(None, 'Congregation', columns, values)
+            return True
+        else:
+            print(missing_fields)
+            return False
