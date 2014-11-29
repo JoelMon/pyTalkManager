6db8f3044cda6c937b7f8bfe37e0e545717ecad4
Joel Montes de Oca <JoelMontes01@gmail.com>
2014-11-29 18:45:46 -0500

    Added test_pyTalkManager.py

---
+++ pyTalkManager/test_pyTalkManager.py
@@ -0,0 +1,12 @@
+__author__ = 'JoelMontes de Oca'
+
+import unittest
+import pyTalkManager as tm
+
+
+class MyTestCase(unittest.TestCase):
+    def test_button_test(self):
+        self.assertEquals(tm.buttonTest(), 'The command worked.')
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
