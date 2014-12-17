aef95a3ddaee5ceb13f0b12345cf8f0c39fb6920
Joel Montes de Oca <JoelMontes01@gmail.com>
2014-12-17 17:43:52 -0500

    added visibility and improved  string conversion.

--- pyTalkManager/db.py
+++ pyTalkManager/db.py
@@ -67,6 +67,8 @@ class DB:
                                            chairman BOOL NOT NULL DEFAULT 0,
                                            coordinator BOOL NOT NULL DEFAULT 0,
                                            note BLOB,
+                                           visibility BOOL NOT NULL DEFAULT
+                                           True,
                                            FOREIGN KEY(congregation)
                                            REFERENCES Congregation(id)
                                            )''')
@@ -134,7 +136,8 @@ class DB:
         """
 
         list_column = ', '.join(column)
-        list_value = "', '".join(value)  # Converts each value into a string.
+        # Convert each value into a string. Join requires strings.
+        list_value = "', '".join(str(v) for v in value)
 
         command = "INSERT INTO {table}({column}) VALUES('{values}')".format(
             table=table,  # adds ' ' to values.
